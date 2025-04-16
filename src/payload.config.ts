a35554775e717c43a54febd294e7c1ff737147fc
DankaMarci <marciemail7@gmail.com>
2025-04-16 11:39:18 +0200

    Changed user slug

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -25,7 +25,7 @@ const dirname = path.dirname(filename)
 
 export default buildConfig({
   admin: {
-    user: Users.slug,
+    user: 'users',
     importMap: {
       baseDir: path.resolve(dirname),
     },
