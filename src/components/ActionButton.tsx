dbbefa489f3e2e958c525d174b1ef411d981d66b
mozsarmate <mozsarmatee@gmail.com>
2025-05-06 18:12:24 +0200

    very nice 404

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,9 +1,10 @@
+"use client";
 import cn from "@/utils/concatenate";
 import React from "react";
 import Link from "next/link";
 
 export default function ActionButton(
-    {onClick, className, children, href}: {
+    {onClick, className, children, href, text}: {
         text?: string;
         onClick?: () => void;
         href?: string;
@@ -15,6 +16,7 @@ export default function ActionButton(
         <button
             className = {cn('m-2 font-bold bg-rajk-green text-foreground py-2 px-4 rounded-md h-fit flex gap-2 items-center hover:bg-green-700 duration-100', className)}
             onClick = {href? () => {} : onClick}>
+            {text}
             {children}
         </button>
     </Link>)
