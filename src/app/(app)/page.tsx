84d035ea9fe165926eef9b4e5bbf091e3a58c525
DankaMarci <marciemail7@gmail.com>
2025-09-07 12:39:26 +0200

    Add getFAQs function to fetch and process localized FAQs

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,10 @@
 import getEvents from "@/payload-find/getEvents";
 import LandingPage from "@/components/LandingPage/LandingPage";
+import getFAQs from "@/payload-find/getFAQs";
 
 export default async function Home() {
     const data = await getEvents()
+    const faq = await getFAQs()
 
     return (
         <LandingPage data={data} />
