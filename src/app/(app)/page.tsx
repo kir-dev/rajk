948caa9a984c9b3cdbf43ff28093a1497b6762fc
DankaMarci <marciemail7@gmail.com>
2025-10-28 18:07:26 +0100

    Remove event data fetching from Home component for simplified rendering

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,10 +1,9 @@
 import LandingPage from "@/components/LandingPage/LandingPage";
-import getEvents from "@/payload-find/getEvents";
 
 export default async function Home() {
-    const data = await getEvents()
+    //const data = await getEvents()
 
     return (
-        <LandingPage data={data} />
+        <LandingPage />
     );
 }
