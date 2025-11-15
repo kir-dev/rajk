a9dba23e2b39d5524d334a8194004dd14bfe56c3
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-11-15 22:51:21 +0100

    Delete migrations for now

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251115_001140_starter_schema from './20251115_001140_starter_schema';
-
-export const migrations = [
-  {
-    up: migration_20251115_001140_starter_schema.up,
-    down: migration_20251115_001140_starter_schema.down,
-    name: '20251115_001140_starter_schema'
-  },
-];
