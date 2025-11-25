583e3a516b99f76ebcc5bfc20fd680ec1d44176b
Tarjányi Csanád <tarjanyicsanad@gmail.com>
2025-11-25 21:37:43 +0100

    Add statistics UI element to awards page

--- src/app/(app)/(default-layout)/dijak/[name]/page.tsx
+++ src/app/(app)/(default-layout)/dijak/[name]/page.tsx
@@ -14,12 +14,13 @@ import {t} from "@/lib/utils";
 import AwardTitleSection from "@/components/Dijak/AwardTitleSection";
 import {notFound} from "next/navigation";
 import AwardHeroSection from "@/components/Dijak/AwardHeroSection";
+import Statistics from "@/components/LandingPage/Statistics";
 
-export default async function Page({ params }: {
+export default async function Page({params}: {
     params: Promise<{ name: string }>
 }) {
 
-    const { name: awardName } = await params;
+    const {name: awardName} = await params;
     const award = await fetchAwardBySlug(awardName);
 
     if (!award) {
@@ -28,49 +29,49 @@ export default async function Page({ params }: {
 
     const awardees = award.awardees && (award.awardees as Awardee[]) || [];
     const currentAwardee = awardees[0];
-    
+
     return (
-        <div className = "flex flex-row h-fit w-full relative bg-black">
+        <div className="flex flex-row h-fit w-full relative bg-black">
             <VertNavBarLayout>
-                <AwardHeroSection award={award} />
+                <AwardHeroSection award={award}/>ú
                 <AwardTitleSection award={award}/>
                 <Section id={"Statistics"} title={"Statistics"} lucideIconName="ChartColumnBig">
-                    {/*<Statistics isLast = {false} color = {"blue"} szin = {"kek"} title = {"Until this year"}
-                                data = {
+                    <Statistics isLast={false} color={"blue"} szin={"kek"} title={"Until this year"}
+                                data={
                                     [
-                                        ["", 28, "awardees"],
+                                        ["", awardees.length, "awardees"],
                                         ["", 156, "course attendee"],
                                         ["", 9846, "lecture participant"]
                                     ]
                                 }
-                    />*/}
-                    <TitleNextToContent title = "Statistics"
-                                        content = "Az elmúlt években retek sokan nyerték meg ezt a díjat." />
-                
+                    />
+                    <TitleNextToContent title="Statistics"
+                                        content="Az elmúlt években retek sokan nyerték meg ezt a díjat."/>
+
                 </Section>
                 {currentAwardee &&
-                    <Section id = {"Latest awardee"} title = {"This year's awardee"} lucideIconName = "User">
-                        <SubTitle text = {"This year's awardee"}></SubTitle>
-                        <div className = "flex flex-col md:flex-row gap-8 items-center w-full justify-center flex-1">
+                    <Section id={"Latest awardee"} title={"This year's awardee"} lucideIconName="User">
+                        <SubTitle text={"This year's awardee"}></SubTitle>
+                        <div className="flex flex-col md:flex-row gap-8 items-center w-full justify-center flex-1">
                             <div>
                                 <Image
-                                    src = {isMedia(currentAwardee!.picture) && (currentAwardee!.picture as Media).url || ""}
-                                    alt = {currentAwardee!.name || "Tiszt Bence"}
-                                    width = {400}
-                                    height = {400}
-                                    className = "rounded-lg border"
+                                    src={isMedia(currentAwardee!.picture) && (currentAwardee!.picture as Media).url || ""}
+                                    alt={currentAwardee!.name || "Tiszt Bence"}
+                                    width={400}
+                                    height={400}
+                                    className="rounded-lg border"
                                 />
                             </div>
                             <div>
-                                <h2 className = "text-3xl font-bold mb-4">{currentAwardee.name}</h2>
+                                <h2 className="text-3xl font-bold mb-4">{currentAwardee.name}</h2>
                                 <RichText className={"w-5/6 wrap-normal"} data={currentAwardee.about}/>
                             </div>
                         </div>
                     </Section>
                 }
                 {awardees &&
-                    <Section id = {"Prev awardees"} title = {"Previous awardees"} className = "mb-40" lucideIconName = "Users">
-                        <SubTitle text = {"Previous awardees"}></SubTitle>
+                    <Section id={"Prev awardees"} title={"Previous awardees"} className="mb-40" lucideIconName="Users">
+                        <SubTitle text={"Previous awardees"}></SubTitle>
                         <AwardeeGrid awardees={awardees}/>
                     </Section>
                 }
